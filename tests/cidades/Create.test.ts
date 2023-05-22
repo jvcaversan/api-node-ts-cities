7f60334c26ad89bc5f567c4041056686e5e7a871
Joao Vitor Caversan <joaovitorcaversan@gmail.com>
2023-05-22 17:53:06 -0300

    add feat: test create

---
+++ tests/cidades/Create.test.ts
@@ -0,0 +1,22 @@
+import { StatusCodes } from "http-status-codes";
+import { testServer } from "../jest.setup";
+
+describe("Cidades - Create", () => {
+  it("Criação de registro", async () => {
+    const res1 = await testServer.post("/cidades").send({
+      nome: "Caxias do Sul",
+    });
+
+    expect(res1.statusCode).toEqual(StatusCodes.CREATED);
+    expect(typeof res1.body).toEqual("number");
+  });
+
+  it("Criação de registro com nome menor que 3 caracteres", async () => {
+    const res1 = await testServer.post("/cidades").send({
+      nome: "Ca",
+    });
+
+    expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+    expect(res1.body).toHaveProperty("errors.body.nome");
+  });
+});
