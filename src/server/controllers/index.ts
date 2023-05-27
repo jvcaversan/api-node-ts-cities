a8ce8e2281a2e05ac8323a5991d571767a46a6e1
Joao Vitor Caversan <joaovitorcaversan@gmail.com>
2023-05-26 23:58:10 -0300

    feat add: controllers pessoas

--- src/server/controllers/index.ts
+++ src/server/controllers/index.ts
@@ -1,3 +1,3 @@
 export * from "./cidades";
-
+export * from "./pessoas";
 //comment
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
-import { CidadesController } from "./../controllers";
+import { CidadesController, PessoasController } from "./../controllers";
 
 const router = Router();
 
@@ -8,6 +8,7 @@ router.get("/", (req, res) => {
   return res.send("ola dev");
 });
 
+//routes cidades
 router.get(
   "/cidades",
   CidadesController.getAllValidation,
@@ -32,6 +33,33 @@ router.delete(
   "/cidades/:id",
   CidadesController.deleteByIdValidation,
   CidadesController.deleteById
+);
+
+//routes pessoas
+router.get(
+  "/pessoas",
+  PessoasController.getAllValidation,
+  PessoasController.getAll
+);
+router.get(
+  "/pessoas/:id",
+  PessoasController.getByIdValidation,
+  PessoasController.getById
+);
+router.post(
+  "/pessoas",
+  PessoasController.createValidation,
+  PessoasController.create
+);
+router.put(
+  "/pessoas/:id",
+  PessoasController.updateByIdValidation,
+  PessoasController.updateById
+);
+router.delete(
+  "/pessoas/:id",
+  PessoasController.deleteByIdValidation,
+  PessoasController.deleteById
 );
 
 export { router };
