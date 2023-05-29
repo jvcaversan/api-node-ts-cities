0fe9f1ea4b73b28ea64907526006826d0dcdbcb8
Joao Vitor Caversan <joaovitorcaversan@gmail.com>
2023-05-29 19:58:10 -0300

    feat: fixing id to consult on db

--- src/server/controllers/cidades/GetAll.ts
+++ src/server/controllers/cidades/GetAll.ts
@@ -30,7 +30,7 @@ export const getAll = async (
     req.query.page || 1,
     req.query.limit || 7,
     req.query.filter || "",
-    Number(req.query.id)
+    Number(req.query.id || 0)
   );
   const count = await CidadesProvider.count(req.query.filter);
 
